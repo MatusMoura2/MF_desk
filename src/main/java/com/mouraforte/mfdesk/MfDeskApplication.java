8231c3e4e2a91b4fe01ee892b7945a9d66fe1893
MatusMoura2 <matus.moura333x2@gmail.com>
2025-04-19 20:28:15 -0300

    iniciando projeto

---
+++ src/main/java/com/mouraforte/mfdesk/MfDeskApplication.java
@@ -0,0 +1,13 @@
+package com.mouraforte.mfdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfDeskApplication.class, args);
+	}
+
+}
